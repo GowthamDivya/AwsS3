7f1ade5663930fb5b29fbbe9187060076cf80484
Gowtham <gurralagowtham@gmail.com>
2024-06-20 23:05:55 +0530

    test db

--- src/main/java/com/vzw/executesync/ExecutesyncApplication.java
+++ src/main/java/com/vzw/executesync/ExecutesyncApplication.java
@@ -1,15 +1,34 @@
 package com.vzw.executesync;
 
+import com.vzw.executesync.common.entities.ExecsyncConfig;
+import com.vzw.executesync.common.repositories.ExecsyncConfigRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExecutesyncApplication {
+import java.util.Optional;
 
+@SpringBootApplication
+public class ExecutesyncApplication implements CommandLineRunner {
+
+    @Autowired
+    ExecsyncConfigRepository execsyncConfigRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ExecutesyncApplication.class, args);
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
+        Optional<ExecsyncConfig> byId = execsyncConfigRepository.findById(1);
+        if (byId.isPresent()) {
+            System.out.println(byId.get());
+        } else {
+            System.out.println("byId = is not found");
+        }
+
+
+    }
 }
